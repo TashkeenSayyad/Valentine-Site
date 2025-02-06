4bf8d0db168bed24996af982b14a0d78fd1a1e6a
SuhabSayyad <tashkeen14@gmail.com>
2025-02-06 23:39:50 +0100

    improve base js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 const messages = [
-    "Are you sure?",
-    "Are you really sure??",
-    "Are you positive?",
-    "Nishu please...",
-    "Just think about it!",
-    "If you say no, I will be sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+  "Are you sure?",
+  "Are you really sure??",
+  "Are you positive?",
+  "Nishu please...",
+  "Just think about it!",
+  "If you say no, I will be sad...",
+  "I will be very sad...",
+  "I will be very very very sad...",
+  "Ok fine, I will stop asking...",
+  "Just kidding, say yes please! ❤️",
 ];
 
 let messageIndex = 0;
 let noClickCount = 0;
 
 function handleNoClick() {
-    const noButton = document.querySelector('.noButton');
-    const yesButton = document.querySelector('.yesButton');
+  const noButton = document.querySelector(".noButton");
+  const yesButton = document.querySelector(".yesButton");
 
-    if (noClickCount < messages.length - 1) {
-        noButton.textContent = messages[messageIndex];
-        messageIndex = (messageIndex + 1) % messages.length;
-        noClickCount++;
-    } else {
-        // Hide the "No" button after final message
-        noButton.style.visibility = 'hidden';
-    }
+  if (noClickCount < messages.length - 1) {
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    noClickCount++;
+  } else {
+    // Hide the "No" button after the final message
+    noButton.style.visibility = "hidden";
+  }
 
-    // Increase Yes button's font size for a fun effect
-    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.3}px`;
+  // Increase the Yes button's font size for a fun effect
+  const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+  yesButton.style.fontSize = `${currentSize * 1.3}px`;
 }
 
 function handleYesClick() {
-    // Redirect to the yes page
-    window.location.href = "yesPage.html";
+  // Redirect to the yes page where the celebration GIF will appear
+  window.location.href = "yesPage.html";
 }
